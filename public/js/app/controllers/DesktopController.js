90126b89a9bcb3bde425805bc8809424b9a578b7
Jonathan El-Bizri <git-commit@elbizri.com>
2014-07-06 18:03:44 -0700

    forced 1 second wait, just to show off the loading spinner

--- public/js/app/controllers/DesktopController.js
+++ public/js/app/controllers/DesktopController.js
@@ -11,8 +11,16 @@ define(['App', 'backbone', 'marionette', 'views/WelcomeView', 'views/DesktopHead
             App.mainRegion.show(new WelcomeView());
 
             App.dataReady.add(function(options){
-                var RatingView = new RatingsView();
-                App.mainRegion.show(RatingView);
+
+            // this is a hack.
+            // I'm so proud of my spinning 'waiting' icon
+            // that I can't bear the idea that a 
+            // speedy DB response will make you miss it.
+            // So, I am forcing the system to wait an extrea second so you see it.
+                window.setTimeout(function () {
+                    var RatingView = new RatingsView();
+                    App.mainRegion.show(RatingView);
+                },1000);
             });
 
         }
